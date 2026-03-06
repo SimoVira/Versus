dc8a2ddf6884ad157ca2d69e7bfd1156346918c6
Virano-Simone <s.virano.3343@vallauri.edu>
2026-03-06 21:02:51 +0100

    Added the library for requests to client-server and modified readme

---
+++ versus-app/api/libreria.ts
@@ -0,0 +1,86 @@
+"use strict";
+const _URL = "http://x.x.x.x:3000"  // IP del tuo portatile dove si trova il server
+
+async function inviaRichiesta(method: any, url: any = "", params: any = {}) {
+	method = method.toUpperCase()
+	url = "/api" + url;
+	let options: any = {
+		"method": method,
+		"headers": {},
+		"mode": "cors",                  // default
+		"cache": "no-cache",             // default
+		"credentials": "omit",    // default
+		"redirect": "follow",            // default
+		"referrerPolicy": "no-referrer", // default no-referrer-when-downgrade
+	}
+
+	if (method == "GET" || method == "DELETE") {
+		options.headers["Content-Type"] = "application/x-www-form-urlencoded"
+		const queryParams = new URLSearchParams();
+		for (let key in params) {
+			let value = params[key];
+			// Notare che i parametri di tipo object vengono serializzati
+			if (value && typeof value === "object")
+				queryParams.append(key, JSON.stringify(value));
+			else
+				queryParams.append(key, value);
+		}
+		if (url.includes("?"))
+			url += "&"
+		else
+			url += "?"
+		url += queryParams.toString()
+	}
+	else {
+		if (params instanceof FormData) {
+			// In caso di formData occorre OMETTERE il Content-Type !
+			// options.headers["Content-Type"]="multipart/form-data;" 
+			options["body"] = params     // Accept FormData, File, Blob			
+		}
+		else {
+			options["body"] = JSON.stringify(params)
+			options.headers["Content-Type"] = "application/json";
+		}
+	}
+
+	// try {
+	// 	const response = await fetch(_URL + url, options)
+	// 	if (!response.ok) {
+	// 		let err = await response.text()
+	// 		return { "status": response.status, err }
+	// 	}
+	// 	else {
+	// 		let data = await response.json().catch(function (err) {
+	// 			console.log(err)
+	// 			return { "status": 422, "err": "Response contains an invalid json" }
+	// 		})
+	// 		return { "status": 200, data }
+	// 	}
+	// }
+	// catch {
+	// 	return { "status": 408, "err": "Connection Refused or Server timeout" }
+	// }
+
+	try {
+		const response = await fetch(_URL + url, options);
+		//leggo il body della risposta come semplice testo
+		const text = await response.text();
+		if (!response.ok) {
+			return { status: response.status, err: text }
+		}
+
+		if (text) {
+			//no errore
+			try {
+				const data = JSON.parse(text);
+				return { status: response.status, data }
+			}
+			catch (err) {
+				return { status: 422, err: "Invalid JSON" }
+			}
+		}
+	}
+	catch (err) {
+		return { status: 408, err: "Connection refused or server timed out" }
+	}
+}
